1a16ef37fb76ac87a381d281badc66a63655e02e
24-arya <sahaarya12@gmail.com>
2024-07-21 11:17:04 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ pip install protobuf==3.20
 pip install --upgrade altair
 pip install altair==4.0.0
 pip install urlextract
-pip install wordcloud
+pip install wordcloud matplotlib
 pip install emoji==1.7.0
